2574d12a91f59005e22f521077f0e24e196c10c3
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-18 17:59:17 +0700

    menambahkan DestinasiHome untuk navigasi home

---
+++ app/src/main/java/com/example/prak8/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak8.uicontroller.route
+
+import com.example.prak8.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
