8a0087c96abb1e2737309ba12fe2e1b9c2163d4b
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-18 19:31:15 +0700

    Konfigurasi MainActivity dengan DataSiswaApp dan tema Compose

--- app/src/main/java/com/example/prak8/MainActivity.kt
+++ app/src/main/java/com/example/prak8/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak8.ui.theme.Prak8Theme
+import com.example.prak8.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prak8Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
