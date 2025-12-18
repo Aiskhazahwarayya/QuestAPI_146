f615cc38565942e1780e138e4ec43e2e6f83a853
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-18 17:57:59 +0700

    menambahkan DestinasiEntry untuk navigasi entry siswa

---
+++ app/src/main/java/com/example/prak8/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.prak8.uicontroller.route
+
+import com.example.prak8.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
