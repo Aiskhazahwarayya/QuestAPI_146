02eff6a7d54cbd547b7386b80e78b4050c814a51
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 16:38:19 +0700

    mengatur padding dan jarak pada layout detail

--- app/src/main/java/com/example/prak8/view/HalamanDetail.kt
+++ app/src/main/java/com/example/prak8/view/HalamanDetail.kt
@@ -1,5 +1,7 @@
 package com.example.prak8.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -83,4 +85,9 @@ private fun BodyDetailDataSiswa(
     statusUIDetail: StatusUIDetail,
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
-){}
+){
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){}
+}
