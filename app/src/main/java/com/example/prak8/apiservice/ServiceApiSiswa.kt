b2d77d47d06f675cb6d5b3813f32158d65d5186c
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 15:13:21 +0700

    menambahkan endpoint API get, edit, dan delete siswa

--- app/src/main/java/com/example/prak8/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/prak8/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.prak8.apiservice
 
 import com.example.prak8.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 
 interface ServiceApiSiswa {
@@ -13,13 +16,13 @@ interface ServiceApiSiswa {
     @POST ("insertTM.php")
     suspend fun postSiswa (@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    //@GET("bacaTeman.php/{id}")
-    //suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
-    //
-    //@PUT("editTM.php/{id}")
-    //suspend fun  editSatuSiswa(@Query("id") id:Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
-    //
-    //@DELETE("deleteTM.php/{id}")
-    //suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
+    @GET("bacaTeman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun  editSatuSiswa(@Query("id") id:Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 
 }
