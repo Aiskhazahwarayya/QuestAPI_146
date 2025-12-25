7697e8cf20754e50d1a94c6fd87dd48cbd0cf670
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 16:27:18 +0700

    menambahkan aksi simpan pada halaman edit siswa

--- app/src/main/java/com/example/prak8/view/HalamanEdit.kt
+++ app/src/main/java/com/example/prak8/view/HalamanEdit.kt
@@ -1,14 +1,17 @@
 package com.example.prak8.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.prak8.uicontroller.route.DestinasiEdit
 import com.example.prak8.viewmodel.EditViewModel
 import com.example.prak8.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,5 +29,20 @@ fun EditSiswaScreen(
             )
         },
         modifier = modifier
-    ){}
+    ){ innerPadding ->
+
+    val coroutineScope = rememberCoroutineScope()
+
+    EntrySiswaBody(
+        uiStateSiswa = viewModel.uiStateSiswa,
+        onSiswaValueChange = viewModel::updateUiState,
+        onSaveClick = {
+            coroutineScope.launch {
+                viewModel.editSatuSiswa()
+                navigateBack()
+            }
+        },
+        modifier = Modifier.padding(innerPadding)
+    )
+    }
 }
