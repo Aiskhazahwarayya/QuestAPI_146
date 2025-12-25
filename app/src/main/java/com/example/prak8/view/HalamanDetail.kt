ca7ae6f248fc19558424ff622513f7b1e97b4738
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 16:47:04 +0700

    mengatur warna card pada detail siswa

--- app/src/main/java/com/example/prak8/view/HalamanDetail.kt
+++ app/src/main/java/com/example/prak8/view/HalamanDetail.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -131,4 +133,12 @@ private fun BodyDetailDataSiswa(
 @Composable
 fun DetailDataSiswa(
     siswa: DataSiswa, modifier: Modifier = Modifier
-) {}
+) {
+
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){}
+}
