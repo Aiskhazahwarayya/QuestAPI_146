18b0ff1adb713a27cb338c8cd958eef5f043421c
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 16:05:36 +0700

    menambahkan  DetailViewModel untuk detail siswa

--- app/src/main/java/com/example/prak8/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/prak8/viewmodel/DetailViewModel.kt
@@ -1,9 +1,26 @@
 package com.example.prak8.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.prak8.modeldata.DataSiswa
+import com.example.prak8.repositori.RepositoryDataSiswa
+import com.example.prak8.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
 }
