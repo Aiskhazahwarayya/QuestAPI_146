d493842ab64a0c97a67c81af8ad2d95aa521eb56
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 15:55:11 +0700

    menambahkan EditViewModel untuk edit satu siswa

---
+++ app/src/main/java/com/example/prak8/viewmodel/EditViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.prak8.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.prak8.modeldata.UIStateSiswa
+import com.example.prak8.modeldata.toUiStateSiswa
+import com.example.prak8.repositori.RepositoryDataSiswa
+import com.example.prak8.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+}
