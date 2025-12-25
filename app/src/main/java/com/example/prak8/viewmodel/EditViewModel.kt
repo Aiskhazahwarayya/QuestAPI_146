d577bfa3c338c1ff7b055967a6b3bdaa0527cdaf
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-12-25 15:59:05 +0700

    menambahkan  validasi input data siswa

--- app/src/main/java/com/example/prak8/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/prak8/viewmodel/EditViewModel.kt
@@ -25,9 +25,13 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
                 .toUiStateSiswa(true)
         }
     }
-
     fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
     }
 
 }
